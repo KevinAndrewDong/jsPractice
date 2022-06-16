793fb0526136db251c2338f4ab35bf68924e24eb
KevinAndrewDong <2970752254@qq.com>
2022-06-17 02:37:07 +0800

    deepclone

---
+++ deepClone.js
@@ -0,0 +1,26 @@
+function deepClone(obj, hash = new WeakMap()) {
+    if (obj === null) return obj; // 如果是null或者undefined我就不进行拷贝操作
+    if (obj instanceof Date) return new Date(obj);
+    if (obj instanceof RegExp) return new RegExp(obj);
+    // 可能是对象或者普通的值  如果是函数的话是不需要深拷贝
+    if (typeof obj !== "object") return obj;
+
+    if (hash.get(obj)) return hash.get(obj);
+    let cloneObj = new obj.constructor();
+    hash.set(obj, cloneObj);
+
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            // 实现一个递归拷贝
+            cloneObj[key] = deepClone(obj[key], hash);
+        }
+    }
+    return cloneObj;
+}
+
+
+let obj = { name: 1, address: { x: 100 } };
+obj.o = obj; // 对象存在循环引用的情况
+let d = deepClone(obj);
+obj.address.x = 200;
+console.log(d);
