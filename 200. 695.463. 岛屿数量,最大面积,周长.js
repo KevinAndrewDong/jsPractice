aa53777ca54ecbe2902c279b27eeee965dd89662
KevinAndrewDong <2970752254@qq.com>
2022-06-26 14:36:43 +0800

    island area and circle

--- 200. 695.463. 岛屿数量,最大面积,周长.js
+++ 200. 695.463. 岛屿数量,最大面积,周长.js
@@ -1,3 +1,44 @@
+/*
+200. 岛屿数量
+给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+
+岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+
+此外，你可以假设该网格的四条边均被水包围。
+ */
+var numIslands = function(grid) {
+    let res = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === '1') {
+                area(grid, i, j);
+                res++;
+            }
+        }
+    }
+    return res;
+};
+
+const area = (grid, row, col) => {
+    if (!inArea(grid, row, col)) return;
+    //找到所有的‘1’，变成‘2’
+    if (grid[row][col] !== '1') return;
+
+    grid[row][col] = '2';
+
+    area(grid, row - 1, col);
+    area(grid, row + 1, col);
+    area(grid, row, col - 1);
+    area(grid, row, col + 1);
+}
+
+const inArea = (grid, row, col) => {
+    return row >= 0 && row < grid.length
+        && col >= 0 && col < grid[0].length;
+}
+
+
+
 /* 695. 岛屿的最大面积
 给你一个大小为 m x n 的二进制矩阵 grid 。
 
