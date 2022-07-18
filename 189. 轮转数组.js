4418699a02ba7a06c2dbaeaa3e4809920264cfd1
KevinAndrewDong <2970752254@qq.com>
2022-07-19 01:34:49 +0800

    feat: rotate-array

---
+++ 189. 轮转数组.js
@@ -0,0 +1,29 @@
+/*
+189. 轮转数组
+给你一个数组，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+
+
+
+示例 1:
+
+输入: nums = [1,2,3,4,5,6,7], k = 3
+输出: [5,6,7,1,2,3,4]
+ */
+
+var rotate = function(nums, k) {
+    let n = nums.length;
+    k = k % n;
+    reverse(nums, 0, n - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, n - 1);
+};
+
+var reverse = (nums, start, end) => {
+    while (start < end) {
+        let temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
