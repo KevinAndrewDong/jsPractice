94a6db74e5f7aa96bdcfaa3ba35ef721f3a807ac
KevinAndrewDong <2970752254@qq.com>
2022-07-20 02:55:12 +0800

    feat: merge intervals

---
+++ 56. 合并区间.js
@@ -0,0 +1,34 @@
+/*
+56. 合并区间
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+
+
+示例 1：
+
+输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出：[[1,6],[8,10],[15,18]]
+解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ */
+
+var merge = function(intervals) {
+    if (intervals.length <= 1) return intervals;
+
+    //按起始点升序
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    let mergeArray = [intervals[0]];
+    let cur = mergeArray[0];
+
+    for (let i = 0; i < intervals.length; i++) {
+        let next = intervals[i];
+        //比较前一个的结束点 和 后一个的起始点
+        if (cur[1] >= next[0]) {
+            cur[1] = Math.max(cur[1], next[1]);
+        } else {
+            cur = next;
+            mergeArray.push(next);
+        }
+    }
+    return mergeArray;
+};
