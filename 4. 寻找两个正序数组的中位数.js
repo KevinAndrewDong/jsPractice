089d606b965bc6eee2a2863c40e6de9ee03b5475
KevinAndrewDong <2970752254@qq.com>
2022-07-20 03:02:16 +0800

    docs : change description

--- 4. 寻找两个正序数组的中位数.js
+++ 4. 寻找两个正序数组的中位数.js
@@ -7,21 +7,29 @@
 
 var findMedianSortedArrays = function(nums1, nums2) {
     const m = nums1.length, n = nums2.length;
-    const left = parseInt((m + n + 1) / 2), right = parseInt((m + n + 2) / 2);
+    const left = parseInt((m + n + 1) / 2);
+    const right = parseInt((m + n + 2) / 2);
 
-    return (findKth(nums1, 0, nums2, 0, left) + findKth(nums1, 0, nums2, 0, right)) * 0.5;
+    return (findKth(nums1, 0, nums2, 0, left)
+        + findKth(nums1, 0, nums2, 0, right))
+        * 0.5;
 };
 
 const findKth = function(nums1, i, nums2, j, k) {
     //num1全淘汰， 结果是nums2第k个
     if (i >= nums1.length) return nums2[j + k - 1];
+    //num2全淘汰， 结果是nums1第k个
     if (j >= nums2.length) return nums1[i + k - 1];
+
     //找第一个
     if (k === 1) return Math.min(nums1[i], nums2[j]);
 
-    let mid1 = (i + parseInt(k / 2) - 1 < nums1.length) ? nums1[i + parseInt(k / 2) - 1] : Number.MAX_SAFE_INTEGER;
-    let mid2 = (j + parseInt(k / 2) - 1 < nums2.length) ? nums2[j + parseInt(k / 2) - 1] : Number.MAX_SAFE_INTEGER;
-    //nums1的前k/2个不满足题意，往后剪枝
+    let mid1 = (i + parseInt(k / 2) - 1 < nums1.length)
+        ? nums1[i + parseInt(k / 2) - 1] : Number.MAX_SAFE_INTEGER;
+    let mid2 = (j + parseInt(k / 2) - 1 < nums2.length)
+        ? nums2[j + parseInt(k / 2) - 1] : Number.MAX_SAFE_INTEGER;
+
+    //nums1的前k/2个不满足题意，舍去，下标往右移动k / 2
     if (mid1 < mid2) {
         return findKth(nums1, i + parseInt(k / 2), nums2, j, k - parseInt(k / 2));
     } else {
