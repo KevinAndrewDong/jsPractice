2994254c4630e18dad5e497e355ec7435fa015e7
KevinAndrewDong <2970752254@qq.com>
2022-07-24 03:08:24 +0800

    add: maximum-length-of-repeated-subarray

--- 718. 最长重复子数组1143. 最长公共子序列.js
+++ 718. 最长重复子数组1143. 最长公共子序列.js
@@ -1,4 +1,35 @@
 /*
+718. 最长重复子数组
+给两个整数数组 nums1 和 nums2 ，返回 两个数组中 公共的 、长度最长的子数组的长度 。
+
+
+
+示例 1：
+
+输入：nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+输出：3
+解释：长度最长的公共子数组是 [3,2,1] 。
+ */
+//子数组连续
+var findLength = function(nums1, nums2) {
+    const m = nums1.length, n = nums2.length;
+    let ans = 0;
+    let dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+    //从1开始，动态规划dp[i][j] = dp[i - 1][j - 1] + 1;
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (nums1[i - 1] == nums2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+    }
+    return ans;
+};
+
+
+
+/*
 1143. 最长公共子序列
 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
 
@@ -15,7 +46,7 @@
 输出：3
 解释：最长公共子序列是 "ace" ，它的长度为 3 。
  */
-
+//子序列不连续
 var longestCommonSubsequence = function(text1, text2) {
     const m = text1.length, n = text2.length;
     const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
