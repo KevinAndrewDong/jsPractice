0ef9b2ab85a5b0d91b02ee1c2bacf7493853dc73
KevinAndrewDong <2970752254@qq.com>
2022-07-24 02:02:06 +0800

    change: description

--- 322. 零钱兑换518. 零钱兑换 II.js
+++ 322. 零钱兑换518. 零钱兑换 II.js
@@ -29,13 +29,9 @@ var coinChange = function(coins, amount) {
 
 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
 
-假设每一种面额的硬币有无限个。 
+假设每一种面额的硬币有无限个。
 
 题目数据保证结果符合 32 位带符号整数。
-
-来源：力扣（LeetCode）
-链接：https://leetcode.cn/problems/coin-change-2
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
 var change = function(amount, coins) {
